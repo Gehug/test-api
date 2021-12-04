3a97f75e457d7e8fd3323bc616a220780ba3c7c2
Gehug <76597702+Gehug@users.noreply.github.com>
2021-12-04 16:07:10 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
+import json
 
 app = Flask(__name__)
 
@@ -7,12 +8,18 @@ input_val = []
 
 @app.route("/", methods=["GET", "POST"])
 def root():
+    app_json = []
 
     if request.method == "POST":
-        input_val.append(request.json)
-        print(input_val)
+        input_val.append(json.dumps(request.json))
 
-    return str(input_val)
+    return render_template("base.html", input_val=input_val)
+@app.route("/reset", methods=["GET"])
+def reset():
+    input_val.clear() 
+    print("ba")
+    return redirect("/")
+    
 
 
 if __name__ == '__main__':
